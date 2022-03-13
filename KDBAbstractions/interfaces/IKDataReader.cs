b50e91d16ae3e360604cae767c5f7e6ee9efea85
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-03-13 15:42:27 -0400

    first commit

---
+++ KDBAbstractions/interfaces/IKDataReader.cs
@@ -0,0 +1,12 @@
+﻿namespace KDBAbstractions.Repository.interfaces
+{
+    public interface IKDataReader
+    {
+        bool YieldedResults { get; }
+
+        int GetInt(string iD_COLUMN);
+        bool Read();
+        string GetString(string aDDRESS_COLUMN);
+        decimal GetDecimal(string v);
+    }
+}
