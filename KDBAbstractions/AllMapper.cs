cc8df81abe3ffaae3d09b8471a1b26159ec3933a
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-03-14 20:22:58 -0400

    added allMapper

---
+++ KDBAbstractions/AllMapper.cs
@@ -0,0 +1,20 @@
+﻿using KDBAbstractions.Repository.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KDBAbstractions
+{
+    public class AllMapper : IKModelMapper
+    {
+        Action<IKDataReader> action;
+        public AllMapper(Action<IKDataReader> action)
+        {
+            this.action = action;
+        }
+        public void map(IKDataReader kdt)
+        {
+            this.action(kdt);
+        }
+    }
+}
